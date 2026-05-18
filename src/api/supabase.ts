0e6d958f86332039aa209c35ffcf74097dba67ba
Hanzell Rivera <hanzellrivera95@gmail.com>
2026-05-18 11:44:23 -0600

    - Add Supabase client setup with secure storage integration

---
+++ src/api/supabase.ts
@@ -0,0 +1,24 @@
+import * as SecureStorage from "expo-secure-store";
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
+
+if (!supabaseUrl || supabaseKey)
+  throw new Error("Missing Supabase enviroment variables");
+
+const SecureStoreAdapter = {
+  getItem: (key: string) => SecureStorage.getItemAsync(key),
+  setItem: (key: string, value: string) =>
+    SecureStorage.setItemAsync(key, value),
+  removeItem: (key: string) => SecureStorage.deleteItemAsync(key),
+};
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: SecureStoreAdapter,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
+})
