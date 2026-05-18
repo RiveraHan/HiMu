aa5d8bc3c7ad7fb6c1639cd49c294093b7e10ca9
Hanzell Rivera <hanzellrivera95@gmail.com>
2026-05-18 15:31:30 -0600

    Add Google client IDs to ProcessEnv interface

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,6 +4,9 @@ declare global {
       EXPO_PUBLIC_SUPABASE_URL: string;
       EXPO_PUBLIC_SUPABASE_KEY: string;
       EXPO_PUBLIC_API_URL: string;
+      EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID: string;
+      EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID: string;
+      EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID: string;
     }
   }
 }
