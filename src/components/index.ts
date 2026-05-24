2aa34f8194a66b06a695a1c8f52688409a4e2923
Hanzell Rivera <hanzellrivera95@gmail.com>
2026-05-23 21:03:30 -0600

    Rename tsx to ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./Atmosphere";
 export * from "./Avatar";
 export * from "./Button";
+export * from "./DJAvatar";
 export * from "./GlassCard";
 export * from "./GlassView";
 export * from "./IconButton";
