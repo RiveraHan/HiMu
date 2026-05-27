b84b6f6f90546cba75b136e5293039868c2d7ca6
Hanzell Rivera <hanzellrivera95@gmail.com>
2026-05-27 16:09:50 -0600

    Refactor supabase client initialization: update import paths and add type parameter for better type safety

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,5 +1,6 @@
+import { secureStorage } from "@/src/lib/secure-storage";
+import { Database } from "@/src/types/database";
 import { createClient } from "@supabase/supabase-js";
-import { secureStorage } from "../lib/secure-storage";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
@@ -7,7 +8,7 @@ const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
 if (!supabaseUrl || !supabaseKey)
   throw new Error("Missing Supabase enviroment variables");
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
   auth: {
     storage: secureStorage,
     autoRefreshToken: true,
